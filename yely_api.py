387e2ad99f3dfa7dc3ae4a3320174f699cf8437b
Omar <Omar>
2021-03-03 14:26:42 -0600

    api call is successful

---
+++ yely_api.py
@@ -0,0 +1,40 @@
+    #possible URL`s
+    # 'https://api.yelp.com/v3/businesses/search'
+    # 'https://api.yelp.com/v3/businesses/matches'
+    # 'https://api.yelp.com/v3/businesses/search/phone'
+    # 'https://api.yelp.com/v3/businesses/{id}'
+    # 'https://api.yelp.com/v3/businesses/{id}/reviews'
+    #inspired by claraj/python_api_workshop
+
+import os 
+import requests
+
+api_key = os.environ.get('YELP_API_KEY')
+
+url = 'https://api.yelp.com/v3/businesses/search'
+
+query = input('What type of restaurants? ')
+
+headers = {'Authorization': 'Bearer ' + api_key}
+params = { 
+    'term' : query,
+    'categories': 'restaurants',
+    'location': 'Minneapolis,MN',
+    'radius': '10000',
+    'offset': 50,
+    'price': 1,
+    'limit': 50
+}
+
+response = requests.get(url, headers=headers, params=params).json()
+
+restaurants = response['businesses']
+
+for r in restaurants:
+    name = r['name']
+    rating = r['rating']
+    location = r['location']
+    display_phone = r['display_phone']
+    address = ','.join(location['display_address'])
+
+    print(f'{name}, {rating}, {address} {display_phone}')
