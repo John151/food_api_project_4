690fe4255f0a20ae0ef6ec0d2774997694f64b59
mania138 <123@fakestreet.com>
2021-03-04 03:16:28 -0600

    added input validation and cleaned up picture_api

--- picture_api.py
+++ picture_api.py
@@ -1,28 +1,16 @@
 import requests
-from pprint import pprint
 import os
-from PIL import Image
+import random
 
-def request_images():
-    search_term = 'pizza'
+
+def request_images(search_term):
+    """takes a search term, requests images from unsplash and picks one out of the top 5 results
+    at random and returns that image after opening"""
+    pick = random.randint(1, 5)
     key = os.environ.get('UNSPLASH_KEY')
     unsplash_url = 'https://api.unsplash.com/search/photos?'
     query = {'query': search_term, 'client_id': key}
     data = requests.get(unsplash_url, params=query).json()
-    image = data['results'][3]['urls']['regular']
-    print(image)
-    # img1 = Image.open(image)
-    # img1.show()
-
-
-
-
-
-# def main():
-#     request_images()
-#
-#
-#
-#
-# if __name__ == '__main__':
-#     main()
+    image = data['results'][pick]['urls']['regular']
+    image_pic = open(image, 'wb')
+    return image_pic
