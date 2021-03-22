cd0f8dabf1c9404b8800e89c78241eed18ca1afa
John L <johnny@example.com>
2021-03-22 14:46:52 -0500

    Added recipe api code to app.py & food.html

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template
 
+import recipe_api
 import picture_api
 import search_util
 import yelp_api
 
+from recipe_api import recipe_call
 from picture_api import request_images
 from search_util import prep_search_term
 from yelp_api import yelp_call
@@ -22,8 +24,9 @@ def get_food():
 
     food_img = picture_api.request_images(search_input)
     food_yelp = yelp_api.yelp_call(search_input)
+    food_recipe = recipe_api.recipe_call(search_input)
 
-    return render_template('food.html', search_term=search_input, food_img=food_img, food_yelp=food_yelp)
+    return render_template('food.html', search_term=search_input, food_img=food_img, food_yelp=food_yelp, food_recipe=food_recipe)
 
 if __name__ == '__main__':
     app.run(debug=True) # turn on developer mode, shows us actual errors when they pop up
