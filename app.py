963d36b9773fdada83c3e90c02af6e34d6e75000
John L <johnny@example.com>
2021-03-27 22:11:09 -0500

    Moved scripts in index to the top of the page and implemented try and except blocks for error handling in app.py

--- app.py
+++ app.py
@@ -21,41 +21,49 @@ def home_page():
 @app.route('/get-food')
 def get_food():
     error = None
-    result = None
 
-    search_input = request.args.get('food_input')
-
-    food_img = picture_api.request_images(search_input)
-    food_yelp = yelp_api.yelp_call(search_input)
-    food_recipe = recipe_api.recipe_call(search_input)
-    recipe_title = food_recipe[0]
-    recipe_ingredients = food_recipe[1]
-    recipe_instructions = food_recipe[2]
+    search_input = request.args.get('food_input').lower()
 
     try:
+        search_term = str(search_input)
+
+        food_img = picture_api.request_images(search_input)
+        food_yelp = yelp_api.yelp_call(search_input)
+        food_recipe = recipe_api.recipe_call(search_input)
+        recipe_title = food_recipe[0]
+        recipe_ingredients = food_recipe[1]
+        recipe_instructions = food_recipe[2]
+
         return render_template(
             'food.html',
-            search_term=search_input, 
+            search_term=search_term, 
             food_img=food_img, 
             food_yelp=food_yelp, 
             recipe_title=recipe_title,
             recipe_ingredients=recipe_ingredients, 
             recipe_instructions=recipe_instructions,
-            result=result,
             search_success=True
         )
-    except ValueError:
+    except ValueError: 
         return render_template(
-            'food.html',
-            search_term=search_input, 
-            food_img=food_img, 
-            food_yelp=food_yelp, 
-            recipe_title=recipe_title,
-            recipe_ingredients=recipe_ingredients, 
-            recipe_instructions=recipe_instructions,
-            result="Bad input",
+            'index.html',
+            search_term=search_term, 
             search_success=False,
             error="Cannot perform search with provided input"
+        )
+    except TypeError: 
+        return render_template(
+            'index.html',
+            search_term=search_term, 
+            search_success=False,
+            error="Cannot perform search with special charcters or numbers"
+        )
+    except IndexError: 
+        return render_template(
+            'index.html',
+            search_term=search_term, 
+            search_success=False,
+            error=f"Food with the name {search_input} cannot be found in the APIs. Try searching a term with no special characters or numbers."
         )
 
 if __name__ == '__main__':
