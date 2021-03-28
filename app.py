faa658e9ab8c7655a00ecea7664ac1bad2e3c6ef
John L <johnny@example.com>
2021-03-27 19:29:58 -0500

    tweaked app.py, changed comment on index

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def home_page():
 """ Page that displays the result of API request """
 @app.route('/get-food')
 def get_food():
+    error = None
+    result = None
+
     search_input = request.args.get('food_input')
 
     food_img = picture_api.request_images(search_input)
@@ -29,9 +32,31 @@ def get_food():
     recipe_ingredients = food_recipe[1]
     recipe_instructions = food_recipe[2]
 
-    return render_template('food.html', search_term=search_input, food_img=food_img, food_yelp=food_yelp, recipe_title=recipe_title,
-                           recipe_ingredients=recipe_ingredients, recipe_instructions=recipe_instructions)
-
+    try:
+        return render_template(
+            'food.html',
+            search_term=search_input, 
+            food_img=food_img, 
+            food_yelp=food_yelp, 
+            recipe_title=recipe_title,
+            recipe_ingredients=recipe_ingredients, 
+            recipe_instructions=recipe_instructions,
+            result=result,
+            search_success=True
+        )
+    except ValueError:
+        return render_template(
+            'food.html',
+            search_term=search_input, 
+            food_img=food_img, 
+            food_yelp=food_yelp, 
+            recipe_title=recipe_title,
+            recipe_ingredients=recipe_ingredients, 
+            recipe_instructions=recipe_instructions,
+            result="Bad input",
+            search_success=False,
+            error="Cannot perform search with provided input"
+        )
 
 if __name__ == '__main__':
     app.run(debug=True) # turn on developer mode, shows us actual errors when they pop up
