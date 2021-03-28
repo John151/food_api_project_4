79b069c19589d98b2731150be08e7f652968991b
mania138 <123@fakestreet.com>
2021-03-27 22:18:18 -0500

    Caching functions happening on all API modules, logic is written to favor cache over API calls

--- picture_api.py
+++ picture_api.py
@@ -1,25 +1,50 @@
 import requests
 import os
 import random
-
+import shelve
 
 
 def request_images(search_term):
-    """takes a search term, requests images from unsplash and picks one out of the top 5 results
-    at random and returns that image after opening"""
-    pick = random.randint(1, 5)
-    key = os.environ.get('UNSPLASH_KEY')
-    unsplash_url = 'https://api.unsplash.com/search/photos?'
-    query = {'query': search_term, 'client_id': key}
-    data = requests.get(unsplash_url, params=query).json()
-    # image = data['results']
-    # print(status)
-    try:
-        image = data['results'][pick]['urls']['regular'] 
-        return image
-        
-    except:
-        image = 'https://images.unsplash.com/photo-1517870662726-c1d98ee36250?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEyMTJ8MHwxfHNlYXJjaHw0fHxlbXB0eSUyMHBsYXRlfGVufDB8fHx8MTYxNjgyMjkzMg&ixlib=rb-1.2.1&q=80&w=1080'
-        return image
+    """ checks cache to see if exact search term has been used before
+    if found, will return that value instead of making the request """    
+    cached_response = check_cache(search_term)
+    if cached_response:
+        print("Item found in cache.\n") # just to keep track of where response is coming from
+        return cached_response
+    else:
+        print('cache not accessed')
+        """ If no result in cache, will takes search term, request image from unsplash and pick one out of the top 5 results
+        at random and returns that result while storing it in cache """
+        pick = random.randint(1, 5)
+        key = os.environ.get('UNSPLASH_KEY') # calls env variable
+        unsplash_url = 'https://api.unsplash.com/search/photos?'
+        query = {'query': search_term, 'client_id': key}
+        data = requests.get(unsplash_url, params=query).json()
+
+        try:
+            image = data['results'][pick]['urls']['regular']
+            add_cache(search_term, image)
+            return image
+            
+        except:
+            image = 'https://images.unsplash.com/photo-1517870662726-c1d98ee36250?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEyMTJ8MHwxfHNlYXJjaHw0fHxlbXB0eSUyMHBsYXRlfGVufDB8fHx8MTYxNjgyMjkzMg&ixlib=rb-1.2.1&q=80&w=1080'
+            return image
+
+""" Checks for search term in cache """     
+def check_cache(search_term):
+    with shelve.open("image_cache") as s:
+        item_found = s.get(search_term)
+        """ if the search term is in cache it will be returned, 
+        if not a msg will print to console, and program will make API call """   
+        if item_found:
+            result = item_found
+            return result
+        else:
+            print('item not found')
 
 
+""" If not already in cache, adds search term and results to cache """
+def add_cache(search_term, image):
+    with shelve.open("image_cache") as s:
+        s[search_term] = image
+        print(search_term, image)
--- yelp_api.py
+++ yelp_api.py
@@ -11,75 +11,73 @@ import requests
 import logging
 import shelve #used for caching api responses 
 
-# This function makes request to the yelp API using the search_term as a parameter
+
 def yelp_call(search_term):
+    """ checks cache to see if exact search term has been used before
+    if found, will return that value instead of making the request """    
     cached_response = check_cache(search_term)
-    
-    if cached_response is not None:
-        print("Item found in cache.\n")
+    if cached_response:
+        print("Item found in cache.\n") # just to keep track of where response is coming from
         return cached_response
-    
-    
-    url = 'https://api.yelp.com/v3/businesses/search'
-    # query = input('What type of restaurants? ')
-    #stores api key
-    key = os.environ.get('YELP_API_KEY')
-    #loggs
-    log = logging.getLogger()
-
-    try:
-        headers = {'Authorization':'Bearer %s'% key} #sets headers 
-        params = { #sets params 
-            'term' : search_term,
-            'categories': 'restaurants',
-            'location': 'Minneapolis,MN',
-            'radius': '10000',
-            'sort_by': 'rating',
-            'open_now': True,
-            'limit': 50
-        }
-
-        response = requests.get(url, headers=headers, params=params).json()
-        restaurants = response['businesses']
-        # data = response.json
-
-        for r in restaurants:
-            name = r['name']
-            rating = r['rating']
-            location = r['location']
-            display_phone = r['display_phone']
-            address = ','.join(location['display_address'])
-
-            return(f'{name}, {rating}, {address} {display_phone}')
-        add_cache(search_term, response)
-        return get_business_name(response)
-  
-    except requests.exceptions.Timeout:
-        log.warning('Request Timed Out: ' + response)
-    except requests.exceptions.TooManyRedirects:
-        log.warning('Error- URL may be bad. Check URL, headers and params')
-    except requests.exceptions.RequestException as e:
-        log.critical('Error - exiting program')
-        
-  #checks for cache      
-def check_cache(search_term):
-    s = shelve.open("yelp_cache")
-
-    item_found = s.get(search_term)
-    s.close()
-    if item_found:
-        result = get_business_name(item_found)
-        return result
     else:
-        return item_found
+        print('cache not accessed')
+    
+        # if no result from cache, make request to the API using the search_term as a parameter
+        url = 'https://api.yelp.com/v3/businesses/search'
+        #stores api key
+        key = os.environ.get('YELP_API_KEY')
+        #loggs
+        log = logging.getLogger()
 
-#adds cache 
+        try:
+            headers = {'Authorization':'Bearer %s'% key} #sets headers 
+            params = { #sets params 
+                'term' : search_term,
+                'categories': 'restaurants',
+                'location': 'Minneapolis,MN',
+                'radius': '10000',
+                'sort_by': 'rating',
+                'open_now': True,
+                'limit': 50
+            }
+
+            response = requests.get(url, headers=headers, params=params).json()
+            restaurants = response['businesses']
+
+            for r in restaurants:
+                name = r['name']
+                rating = r['rating']
+                location = r['location']
+                display_phone = r['display_phone']
+                address = ','.join(location['display_address'])
+                yelp_results = f'{name}, rated {rating} stars, {address}, {display_phone}'
+                add_cache(search_term, yelp_results)
+                return yelp_results
+            
+        except requests.exceptions.Timeout:
+            log.warning('Request Timed Out: ' + response)
+        except requests.exceptions.TooManyRedirects:
+            log.warning('Error- URL may be bad. Check URL, headers and params')
+        except requests.exceptions.RequestException as e:
+            log.critical('Error - exiting program')
+        
+""" Checks for search term in cache """      
+def check_cache(search_term):
+    with shelve.open("yelp_cache") as s:
+        item_found = s.get(search_term)
+        """ if the search term is in cache it will be returned, 
+        if not a msg will print to console, and program will make API call """
+        if item_found:
+            result = item_found
+            return result
+        else:
+            print('item not found')
+
+
+""" If not already in cache, adds search term and results to cache """
 def add_cache(search_term, data):
-    s = shelve.open("yelp_cache")
-    s[search_term] = data
+    with shelve.open("yelp_cache") as s:
+        s[search_term] = data
+        print(search_term, data)
 
 
-#This function gets data and returs the closest and first bussiness name from api response
-def get_business_name(data):
-    business_name = data['businesses'][0]['name']
-    return business_name
